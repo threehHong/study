156f2be52ce83ad36f420245a479f77163837252
threehHong <honghyohun@naver.com>
2022-08-25 20:53:38 +0900

    green

--- 001-2.JS응용/js/main.js
+++ 001-2.JS응용/js/main.js
@@ -41,6 +41,92 @@ function loadFn() {
     // 1. 호출확인
     console.log("로딩완료!");
 
+    // 2. 대상 선정 
+    // 이벤트 대상 : .abtn
+    let abtn = document.querySelectorAll(".abtn");
+    // 변경 대상 : #slide
+    let slide = document.querySelector("#slide");
+
+    // 광클금지 상태변수
+    let prot = 0; // 1-금지, 0-허용
+
+    // 3. 클릭 이벤트 기능 구현
     
+    // 3-1 오른쪽 버튼
+    abtn[1].onclick = () => {
+
+        ////////// 광클금지 //////////
+        if(prot) return; // 리턴으로 나감!
+        // prot === 1 이라고 안해도 1이면 true!
+        // 1은 true, 0은 false와 서로 교차됨!
+        prot = 1; // 첫번째 신호가 잠금!
+        // 0.4초 후 해제
+        setTimeout(() => prot = 0, 400);
+        ////////// 광클 금지 //////////
+
+        // 1. 호출 확인
+        console.log("오른쪽 버튼!");
+        // 2. 대상 선정 : #silide -> slide 변수
+        // 3. 기능 구현 : left값을 -100$로 변경
+        // 기능 1단계 - 왼쪽으로 슬라이드 하나만큼 나가기! + 트랜지션
+        slide.style.left = '-100%';
+        slide.style.transition = 'left .4s ease-in-out'; // .은 영을 생략하면 사용 
+        // 기능 2단계 - 슬라이드 이동후 맨 앞 li 맨뒤이동
+        // 0.4초 후 실행하려면? setTimeout(함수, 시간)
+        setTimeout(()=>{
+            // 맨앞요소 맨뒤이동
+            slide.appendChild(
+                slide.querySelectorAll('li')[0]);
+            // 동시에 -100%인 slide의 left 값을 0으로 변경
+            slide.style.left = '0'; 
+            // left가 0이될 때 트랜지션 지우기!
+            slide.style.transition = 'none';
+        }, 400); // 400은 0.4초 후.
+    };
+    
+    // 3-2 왼쪽 버튼
+    abtn[0].onclick = () => {
+
+        ////////// 광클금지 //////////
+        if(prot) return; // 리턴으로 나감!
+        // prot === 1 이라고 안해도 1이면 true!
+        // 1은 true, 0은 false와 서로 교차됨!
+        prot = 1; // 첫번째 신호가 잠금!
+        // 0.41초 후 해제
+        setTimeout(() => prot = 0, 410);
+        ////////// 광클 금지 //////////
+
+        // 1. 호출확인
+        console.log("왼쪽 버튼!");
+        // 2. 대산선정: #slide -> slide변수
+        // 3. 기능구현
+        // 기능구현 1단계
+        // li 요소
+        let lis = slide.querySelectorAll('li');
+        // 맨뒤요소 맨앞 이동 : insertBefore(넣을놈, 넣을놈전놈);
+        slide.insertBefore(lis[lis.length-1], lis[0]);
+        // left 값을 -100%로 변경하기
+        slide.style.left = '-100%';
+        // 트랜지션 없애기
+        slide.style.transition = 'none';
+
+        // 기능구현 2단계
+        // 실행시간을 조금 차이를 준다!
+        // setTimeout() 사용!
+        setTimeout(() => {
+            // left값을 0으로 변경
+            slide.style.left = '0';
+            //  트랜지션 설정
+            slide.style.transition = 'left .4s ease-in-out';
+        }, 10);
+        // left값을 0으로 변경
+        slide.style.left = '0';
+        // 트랜지션 설정
+        slide.style.transition = 'left .4s ease-in-out';
+
+        // 기능구현 3단계
+        // left값을 0으
+    };
+
 } //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
