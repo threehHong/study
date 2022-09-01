0221ed74181e3b944cc785b98fd56d9cdc18226e
threehHong <honghyohun@naver.com>
2022-09-01 21:45:10 +0900

    green

--- 002.jQuery학습/002. 응용연습/002.site/js/linksys.js
+++ 002.jQuery학습/002. 응용연습/002.site/js/linksys.js
@@ -33,7 +33,38 @@ $(() => { ////////// jQB ///////////////////////
         GNB 메뉴 파트 링크 셋팅하기
     ************************************/
     // 대상: .gnb a
-    
+    $('.gnb a').click((e) => { // e는 이벤트 전달 변수
+        
+        // 기본 이동 막기
+        e.preventDefault();
+        
+        // 화살표함수일 때 나 자신
+        let ele = e.currentTarget;
+
+        // console.log(ele);
+
+        // 1. 클릭된 a요소의 클자 읽기
+        let txt = $(ele).text().toLowerCase().trim();
+        // toLowerCase() 소문자변환
+        // 참고) toUpperCase() 대문자변환
+        // trim() 앞뒤공백제거
+
+        console.log(txt);
+
+        // 2. 서브페이지 이동하기
+        if(txt !== 'search'){
+            location.href = 'category.html?cat=' + encodeURIComponent(txt);
+        }
+
+        // 카테고리를 구분하기 위한 파라미터 키=값 쌍을 보냄
+        // cat=카테고리명
+        // 이것을 받아서 페이지 셋업을 함!
+        // 이렇게 데이터를 url로 전달하는 방식을 GET 방식이라고 함
+        // 특수문자가 있으므로 (time & gem) 이것을 보낼 때 
+        // encodeURIComponent() 로 변환하여 보내고
+        // 받는 곳에서는 decodeURIComponent() 로 복원함!
+
+    }); /////// click ///////
 
 
 
