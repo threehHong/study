b22cf138c789ab22b0ca44edcd2d398a50e16d52
threehHong <honghyohun@naver.com>
2022-09-11 14:53:14 +0900

    study

--- 03 JavaScript/22 classList/00 classList.js
+++ 03 JavaScript/22 classList/00 classList.js
@@ -19,6 +19,10 @@ toggleBtn.addEventListener('click', function(){
     textH2.classList.toggle('text') // 
 }) // toggle 버튼 클릭시 타겟 태그 class에 toggle() 메소드의 괄호안 class명이 없을 경우 추가, 있을 경우 삭제를 한다.
 
+
+// contains
+console.log(textH1.classList.contains('text'));
+
 /*
 
 1.
@@ -38,6 +42,10 @@ toggle (토글 스위치 생각하기)
 target.classList.toggle("class 속성에 추가하거나 삭제될 속성값")
 toggle 버튼 클릭시 타겟 태그 class에 toggle() 메소드의 괄호안 class명이 없을 경우 추가, 있을 경우 삭제하는 메소드.
 
+
+target.classList.contains("class명");
+타겟 태그 class에 contatins()의 대괄호 안 클래스명이 존재하는지 확인하는 메소드
+존재하면 true, 존재하지 않으면 false 반환.
 
 */  
 
