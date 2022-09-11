fe149a2b400d6368acb9e58368281d5d532b3195
threehHong <honghyohun@naver.com>
2022-09-11 14:31:17 +0900

    study

--- 03 JavaScript/22 classList/00 classList.js
+++ 03 JavaScript/22 classList/00 classList.js
@@ -17,11 +17,9 @@ removeBtn.addEventListener("click", function(){
 }) // remove 버튼 클릭시 주어진 문자열 값을 class 속성의 속성값에서 삭제한다
 toggleBtn.addEventListener('click', function(){
     textH2.classList.toggle('text') // 
-}) // toggle 버튼 클릭시 주어진 문자열 값을 class 속성의 속성값으로 추가한다.
-   // toggle 버튼을 한번더 클릭시 주어진 문자열 값을 class 속성의 속성값에서 삭제한다.
-   // 처음 클릭시 추가, 두번째 클릭시 삭제.
+}) // toggle 버튼 클릭시 타겟 태그 class에 toggle() 메소드의 괄호안 class명이 없을 경우 추가, 있을 경우 삭제를 한다.
 
-/* 
+/*
 
 1.
 target.classList
@@ -38,6 +36,8 @@ target 태그의 class 속성에 저장된 속성값을 삭제하는 메소드
 
 toggle (토글 스위치 생각하기)
 target.classList.toggle("class 속성에 추가하거나 삭제될 속성값")
-target 태그의 class 속성에 속성값을 추가하거나 삭제하는 메소드
+toggle 버튼 클릭시 타겟 태그 class에 toggle() 메소드의 괄호안 class명이 없을 경우 추가, 있을 경우 삭제하는 메소드.
+
 
 */  
+
