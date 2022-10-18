afdda8797e3a772d79ae9ff63184d00f6ccea144
threehHong <honghyohun@naver.com>
2022-10-18 20:43:04 +0900

    study

--- 08 programmers/programmers.js
+++ 08 programmers/programmers.js
@@ -1,7 +1,18 @@
 /* 코딩 테스트 연습/Lv.0/두 수의 곱. (22.10.17)  */
-function solution(num1, num2) {
+/* function solution(num1, num2) {
     if(num1 <= 100 && num1>=0 && num2 <= 100 && num2>=0){
         let answer = (num1 * num2);
         return answer;
     }
-}
+} */
+
+/* 코딩 테스트 연습/Lv.0/두 수의 차. (22.10.18)  */
+/* function solution(num1, num2) {
+    if(num1 <= 50000 && num1>=-50000 && num2 <= 50000 && num2>=-50000){
+        let answer = (num1 - num2);
+        return answer;
+    } else {
+        let error = "error";
+        console.log(error);
+    }
+} */
