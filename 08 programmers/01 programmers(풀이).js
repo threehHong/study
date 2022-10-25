4c24d5c34ee94f25322349fe32b50623d02b1e02
threehHong <honghyohun@naver.com>
2022-10-25 20:50:20 +0900

    study

--- 08 programmers/01 programmers(풀이).js
+++ 08 programmers/01 programmers(풀이).js
@@ -64,7 +64,7 @@
 } */
 
 /* 코딩 테스트 연습/ Lv.0 / 각도기. (22.10.24)  */
-function solution(angle) {
+/* function solution(angle) {
     if(angle % 1 != 0){
         console.log('정수를 입력하세요');
     } else{
@@ -79,6 +79,53 @@ function solution(angle) {
         } 
         if (angle == 180){
             return 4;
+        }.
+    }
+} */
+
+/* 코딩 테스트 연습/ Lv.0 / 두 수의 나눗셈. (22.10.25)  */
+/* function solution(num1, num2) {
+    if(num1 <= 100 && num1 > 0 && num2 <= 100 && num2 > 0){
+        let answer = Math.floor((num1/num2) * 1000);
+        return answer;
+    } else {
+        console.log('error');
+    }
+} */
+
+/* 코딩 테스트 연습/ Lv.0 / 분수의 덧셈. (22.10.25)  */
+function solution(denum1, num1, denum2, num2) {
+    if(0 < denum1 < 1000 && 0 < num1 < 1000 && 0 < denum2 < 1000 && 0 < num2 < 1000) {
+        let answer = [];
+        numerator = (denum1 * num2) + (denum2 * num1);
+        denominator = num1 * num2;
+
+        answer[0] = numerator;
+        answer[1] = denominator;
+
+        let maxnum = 0;
+        let loop = 1;
+        
+
+        for(i = 0; i<= numerator; i++){
+            if(answer[0]%i === 0 || answer[1]%i === 0){
+                maxnum = i;
+            }
         }
+        while(loop){
+            if(answer[0]/maxnum != 0 || answer[1]/maxnum != 0){
+                loop = 0;
+            } else {
+                answer[0] = answer[0]/maxnum ;
+                answer[1] = answer[1]/maxnum ;
+            }
+        }
+        return answer;
+    } else {
+        console.log('error');
     }
 }
+
+console.log(solution(4,3,2,1));
+
+
