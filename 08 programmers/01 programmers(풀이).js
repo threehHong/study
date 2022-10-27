20bf535c76cd45fa98cf7007d5c7b18d1e7e3589
highanddry1 <honghyohun@naver.com>
2022-10-28 00:19:49 +0900

    study

--- 08 programmers/01 programmers(풀이).js
+++ 08 programmers/01 programmers(풀이).js
@@ -93,8 +93,8 @@
     }
 } */
 
-/* 코딩 테스트 연습/ Lv.0 / 분수의 덧셈. (22.10.25)  */
-function solution(denum1, num1, denum2, num2) {
+/* 코딩 테스트 연습/ Lv.0 / 분수의 덧셈. (22.10.25) 테스트는 통과, 제출 후 채점하기는 X 정확성 33.3프로 */
+/* function solution(denum1, num1, denum2, num2) {
     if(0 < denum1 < 1000 && 0 < num1 < 1000 && 0 < denum2 < 1000 && 0 < num2 < 1000) {
         numerator = (denum1 * num2) + (denum2 * num1);
         denominator = num1 * num2;
@@ -103,11 +103,12 @@ function solution(denum1, num1, denum2, num2) {
         answer[0] = numerator;
         answer[1] = denominator;
 
+        numerator > denominator ? comp = numerator : comp = denominator;
         let maxnum = 1;
         let loop = 1;
         let i = 2;
 
-        for(i = 2; i <= numerator; i++){
+        for(i = 2; i <= comp; i++){
             while(loop){
                 a = answer[0]/i
                 b = answer[1]/i
@@ -129,9 +130,49 @@ function solution(denum1, num1, denum2, num2) {
     } else {
         console.log('error');
     }
-}
+} */
 
-console.log(solution(9,2,1,3));
-/* console.log(1/2); */
+/* 코딩 테스트 연습/ Lv.0 / 분수의 덧셈. (22.10.27) */
+/* GCD : Greatest Common Divisor */
+// function solution(denum1, num1, denum2, num2) {
+//     if(0 < denum1 < 1000 && 0 < num1 < 1000 && 0 < denum2 < 1000 && 0 < num2 < 1000) {
+//         let numerator = (denum1 * num2) + (denum2 * num1);
+//         let denominator = num1 * num2;
+//         let answer = [];
+        
+//         /* 최대공약수 구하는 공식 밑에 내용 암기하고 다른 풀이 추후 탐구 및 정리(해당 문제에서 다른 사람의 풀이 보기 + 구글링) */
+           /* 나머지 수를 결국 둘 중 최솟값에 나눈 나머지가 0이 되어야 최대공약수가 되는게 아닐까 싶음 이라는 의견 있음 */
+           /* a % b에서 a<b일 경우 a % b의 결과 값은 a 이다 */
+//         let GCD = (a, b) => {
+//             return a % b === 0 ? b : GCD(b, a % b)
+//         };
 
+//         let gcd = GCD(numerator, denominator);
+    
+//         console.log(gcd)
+    
+//         answer[0] = numerator / gcd;
+//         answer[1] = denominator / gcd;
+    
+//         return answer;
+//     }
+// }
+
+/* 최초 gcd(3, 5)호출 기준으로
+
+1. 3 % 5 = 3이므로 재귀함수 gcd(5, 3)
+2. 5 % 3 = 2이므로 gcd(3, 2)
+3. 3 % 2 = 1이므로 gcd(2, 1)
+4. 2 % 1 = 0이므로 gcd(1, 0)
+5. num2가 0이므로 num1인 1을 return
+
+gcd(3, 5)는 결과는 gcd(5, 3), 
+gcd(5, 3)의 결과는 gcd(3, 2), 
+gcd(3, 2)의 결과는  gcd(2, 1),
+gcd(2, 1)의 결과는 gcd(1, 0),
+gcd(1, 0)의 결과값은 1이므로 결과적으로 gcd(3, 5)의 값도 1이됨
+
+즉, gcd(1, 0)은 1이니까 gcd(1, 0) === 1을 반환하는 gcd(2, 1)의 값도 1,
+gcd(2, 1)을 반환하는 gcd(3, 2)의 값도 1,
+타고타고 가서 결과적으로 gcd(3, 5)의 값은 1이 나옴 */
 
