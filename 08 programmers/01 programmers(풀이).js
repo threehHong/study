b92246e428feb1005c2fb144d12ade911ddd3618
threehHong <honghyohun@naver.com>
2022-11-05 18:46:47 +0900

    study

--- 08 programmers/01 programmers(풀이).js
+++ 08 programmers/01 programmers(풀이).js
@@ -303,18 +303,19 @@ console.log(solution([1,3,5,7])); */
 }
 console.log(solution([1,2,100,-99,1,2,3])); */
 
-/* 코딩 테스트 연습/ Lv.0 / 배열 뒤집기 (22.11.01) 오답 다시 풀어야 함 */
-/* function solution(num_list) {
+/* 코딩 테스트 연습/ Lv.0 / 문자열 뒤집기 (22.11.01) 오답 다시 풀어야 함 */
+function solution(num_list) {
     let answer = [];
 
     for(i=0; i < num_list.length; i++){
         answer[i] = num_list[num_list.length-1-i]
     }
+    console.log(answer)
 
     return answer;
 }
 
-console.log(solution([1,2,3,4,5])) */
+console.log(solution("jaron"))
 
 /* 코딩 테스트 연습/ Lv.0 / 배열 원소의 길이 (22.11.05) */
 /* function solution(strlist) {
